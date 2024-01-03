46cd779b7df70ffcccfe3aad38f68c670b3441bb
Manuel Dinis Júnior <91975829+manueldinisjunior@users.noreply.github.com>
2024-01-03 12:29:25 +0200

    Skyscanner task completition, refactoring

--- template/src/App.js
+++ template/src/App.js
@@ -1,28 +1,81 @@
-import React from 'react';
-import { BpkCode } from '@skyscanner/backpack-web/bpk-component-code';
-import BpkButton from '@skyscanner/backpack-web/bpk-component-button';
-import BpkText from '@skyscanner/backpack-web/bpk-component-text';
-
-import { cssModules } from '@skyscanner/backpack-web/bpk-react-utils';
-
+import React, { Component } from 'react';
+import { BpkCode } from 'bpk-component-code';
+import BpkButton from 'bpk-component-button';
+import BpkText from 'bpk-component-text';
+import BpkCalendar, { CALENDAR_SELECTION_TYPE } from 'bpk-component-calendar';
+import format from 'date-fns/format';
 import STYLES from './App.scss';
 
-const getClassName = cssModules(STYLES);
+// Utility function for classnames
+const c = className => STYLES[className] || 'UNKNOWN';
 
-const App = () => (
-  <div className={getClassName('App')}>
-    <header className={getClassName('App__header')}>
-      <div className={getClassName('App__header-inner')}>
-        <BpkText tagName="h1" textStyle="xxl" className={getClassName('App__heading')}>Welcome to React + Backpack</BpkText>
+// Utility functions for date formatting
+const formatDateFull = date => format(date, 'EEEE, do MMMM yyyy');
+const formatMonth = date => format(date, 'MMMM yyyy');
+
+// Days of the week configuration
+const daysOfWeek = [
+  { name: 'Sunday', nameAbbr: 'Sun', index: 0, isWeekend: true },
+  { name: 'Monday', nameAbbr: 'Mon', index: 1, isWeekend: false },
+  { name: 'Tuesday', nameAbbr: 'Tues', index: 2, isWeekend: false },
+  { name: 'Wednesday', nameAbbr: 'Wed', index: 3, isWeekend: false },
+  { name: 'Thursday', nameAbbr: 'Thur', index: 4, isWeekend: false },
+  { name: 'Friday', nameAbbr: 'Fri', index: 5, isWeekend: false },
+  { name: 'Saturday', nameAbbr: 'Sat', index: 6, isWeekend: true },
+];
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      selectionConfiguration: {
+        type: CALENDAR_SELECTION_TYPE.single,
+        date: null,
+      },
+    };
+  }
+
+  // Handle date selection
+  handleDateSelect = date => {
+    this.setState({
+      selectionConfiguration: {
+        type: CALENDAR_SELECTION_TYPE.single,
+        date: date,
+      },
+    });
+  };
+
+  render() {
+    return (
+      <div className={c('App')}>
+        <header className={c('App__header')}>
+          <div className={c('App__header-inner')}>
+            <BpkText tagName="h1" textStyle="xxl" className={c('App__heading')}>
+              Reservation Date
+            </BpkText>
+          </div>
+        </header>
+        <main className={c('App__main')}>
+          <div>
+            <BpkCalendar
+              id="calendar"
+              onDateSelect={this.handleDateSelect}
+              formatMonth={formatMonth}
+              formatDateFull={formatDateFull}
+              daysOfWeek={daysOfWeek}
+              weekStartsOn={0}
+              changeMonthLabel="Change month"
+              nextMonthLabel="Next month"
+              previousMonthLabel="Previous month"
+              selectionConfiguration={this.state.selectionConfiguration}
+            />
+          </div>
+          <BpkButton onClick={() => alert('It works!')}>Continue</BpkButton>
+        </main>
       </div>
-    </header>
-    <main className={getClassName('App__main')}>
-      <BpkText tagName="p" className={getClassName('App__text')}>
-        To get started, edit <BpkCode>src/App.jsx</BpkCode> and save to reload.
-      </BpkText>
-      <BpkButton onClick={() => alert('It works!')}>Click me</BpkButton>
-    </main>
-  </div>
-);
+    );
+  }
+}
 
 export default App;
